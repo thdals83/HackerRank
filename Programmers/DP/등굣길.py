f14f2c1f19eb30ca7dacc03a082980fa7f372b80
thdals83 <thdals83@naver.com>
2021-02-01 16:33:45 +0900

    프로그래머스 DP(등굣길)

---
+++ Programmers/DP/등굣길.py
@@ -0,0 +1,27 @@
+def solution(m, n, puddles):
+    dp=[[0 for _ in range(m+1)] for _ in range(n+1)]
+
+    dp[1][1]=1
+
+
+    for i in range(1,n+1):
+        for j in range(1, m+1):
+            if i==1 and j==1:
+                continue
+            if [j,i] in puddles:
+                dp[i][j]=0
+            else:
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+    for i in range(1,n+1):
+        for j in range(1, m+1):
+            print(dp[i][j],end="")
+
+        print()
+    return dp[n][m]% 1000000007
+
+
+if __name__ == "__main__":
+    m=4
+    n=3
+    puddles=[[2,2]]
+    print(solution(m,n,puddles))
